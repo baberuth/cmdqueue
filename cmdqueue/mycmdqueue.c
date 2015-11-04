deaaf62edbf6c14f78f1dd2cef35d0d748f96a69
Bas van Tiel <bvantiel@gmail.com>
2015-11-04 21:13:43 +0100

    Added a mycmdqueue

---
+++ cmdqueue/mycmdqueue.c
@@ -0,0 +1,105 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include "cmdqueue.h"
+#include "mycmdqueue.h"
+
+struct mycmdqueue_tag {
+    cmdqueue_t cmdqueue;
+    void *cookie;
+    int32_t (*cmd_callback)(void *cookie, int32_t callback_type, void *data, int32_t len);
+};
+
+static void mycmd_callback(void *cookie, struct cmd *cmd)
+{
+    struct mycmd * mycmd = (struct mycmd *)cmd;
+    switch (mycmd->type) {
+        case INIT:
+        {
+        }
+        break;
+        case DEINIT:
+        {
+        }
+        break;
+        case START:
+        {
+        }
+        break;
+        case STOP:
+        {
+        }
+        break;
+    }
+}
+
+int32_t mycmdqueue_init(mycmdqueue_t * handle, 
+                        void *cookie, 
+                        int32_t (*mycmdqueue_callback)(void *cookie,
+                                                       int32_t callback_type,
+                                                       void *data,
+                                                       int32_t len))
+{
+    *handle = malloc(sizeof(struct mycmdqueue_tag));
+
+    if (*handle) {
+        const int32_t num_commands = 100;
+        mycmdqueue_t phandle = *handle;
+        struct mycmd *cmd;
+
+        cmdqueue_init(&phandle->cmdqueue,
+                      "mycmdqueue",
+                      mycmd_callback,
+                      phandle,
+                      num_commands,
+                      sizeof(struct mycmd));
+
+        cmdqueue_getcmd_sync(phandle->cmdqueue, (struct cmd **)&cmd);
+        cmd->type = INIT;
+        cmdqueue_sync_cmd(phandle->cmdqueue, &cmd->cmd);
+
+        phandle->cookie           = cookie;
+        phandle->cmd_callback     = mycmdqueue_callback;
+
+    }
+    return 0;
+}
+
+int32_t mycmdqueue_deinit(mycmdqueue_t handle)
+{
+    struct mycmd *cmd;
+
+    if (!handle) return 1;
+
+    cmdqueue_getcmd_sync(handle->cmdqueue, (struct cmd **)&cmd);
+    cmd->type = DEINIT;
+    cmdqueue_sync_cmd(handle->cmdqueue, &cmd->cmd);
+
+    cmdqueue_deinit(handle->cmdqueue);
+    free(handle);
+
+    return 0;
+}
+
+int32_t mycmdqueue_start(mycmdqueue_t handle)
+{
+    struct mycmd *cmd;
+
+    cmdqueue_getcmd_sync(handle->cmdqueue, 
+                         (struct cmd **)&cmd);
+
+    cmd->type = START;
+
+    return cmdqueue_sync_cmd(handle->cmdqueue, &cmd->cmd);
+}
+
+int32_t mycmdqueue_stop(mycmdqueue_t handle)
+{
+    struct mycmd *cmd;
+
+    cmdqueue_getcmd_sync(handle->cmdqueue, 
+                         (struct cmd **)&cmd);
+
+    cmd->type = STOP;
+
+    return cmdqueue_sync_cmd(handle->cmdqueue, &cmd->cmd);
+}
---
+++ cmdqueue/mycmdqueue.h
@@ -0,0 +1,47 @@
+#ifndef MYCMDQUEUE_H
+#define MYCMDQUEUE_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    enum mycmd_type {
+        INIT,
+        DEINIT,
+        START,
+        STOP,
+    };
+
+    struct mycmd {
+        struct cmd cmd;
+        int32_t type;
+        union {            
+            struct {
+                int32_t dontcare;
+            } mycmd_start;
+
+            struct {
+                int32_t dontcare;
+            } mycmd_stop;
+        } mycmd;
+    };
+
+    typedef struct mycmdqueue_tag * mycmdqueue_t;
+
+    int32_t mycmdqueue_init(mycmdqueue_t * handle, 
+                            void *cookie, 
+                            int32_t (*mycmdqueue_callback)(void *cookie,
+                                                           int32_t callback_type,
+                                                           void *data,
+                                                           int32_t len));
+    int32_t mycmdqueue_deinit(mycmdqueue_t handle);
+    int32_t mycmdqueue_start(mycmdqueue_t handle);
+    int32_t mycmdqueue_stop(mycmdqueue_t handle);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
